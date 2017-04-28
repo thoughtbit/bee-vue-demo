3684b3bc8020412e2711bafae92cee02e05e6c7f
moocss <moocss@gmail.com>
2017-04-28 18:30:16 +0800

    vue jsx

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import HelloJSX from '@/components/HelloJSX'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello,
+    },
+    {
+      path: '/jsx',
+      name: 'HelloJSX',
+      component: HelloJSX,
+    },
+  ],
+})
