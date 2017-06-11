3ea58f11b27bc78237ce1682087a1b35a45de7e1
moocss <moocss@gmail.com>
2017-06-11 22:21:01 +0800

    sw

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,6 +3,10 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="#/jsx">JSX</a></li>
+    </ul>
+    <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
