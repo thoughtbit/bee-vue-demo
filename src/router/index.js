a852ace87e890f2c6d9ec4bd7563a936d3144eae
moocss <moocss@gmail.com>
2017-07-03 15:58:53 +0800

    webpack3.0 dll bug修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import VueRouter from 'vue-router';
 import Hello from '~/components/Hello';
 import HelloJSX from '~/components/HelloJSX';
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
+export default new VueRouter({
   routes: [
     {
       path: '/',
